ee651e76f2f2de0169444f06bd029c6387e52c50
Gemma Standley <gjs97@cornell.edu>
2019-04-09 17:18:12 -0400

    changed merlin file

--- engine.ml
+++ engine.ml
@@ -1,10 +1,11 @@
 open ANSITerminal
 
 let play_game =
+  failwith "Unimplemented"
 
 let main () = 
   ANSITerminal.(print_string[red] "\n\ Welcome to Snake! Press enter to start. 
-    \n")
+    \n");
   match read_line () with
   | exception End_of_file -> ()
   | _ -> play_game 
