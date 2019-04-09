e5203d3eae8037edece305a8d2db5065cc6d8ce6
Gemma Standley <gjs97@cornell.edu>
2019-04-09 17:14:00 -0400

    started working

---
+++ engine.ml
@@ -0,0 +1,12 @@
+open ANSITerminal
+
+let play_game =
+
+let main () = 
+  ANSITerminal.(print_string[red] "\n\ Welcome to Snake! Press enter to start. 
+    \n")
+  match read_line () with
+  | exception End_of_file -> ()
+  | _ -> play_game 
+
+let () = main ()
