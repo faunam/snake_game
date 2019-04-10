c8fc7ce169d44d3d1876ea8b9cc357efec384e71
Yunshan Liu <yl2978@cornell.edu>
2019-04-10 13:08:46 -0400

    modified the structure and representation

--- main.ml
+++ main.ml
@@ -6,87 +6,94 @@ open ANSITerminal
 (*let make_snake = 
   failwith "unimplemented"
 
-let make_apple = 
+  let make_apple = 
   failwith "unimplemented"
 *)
 (*returns a string of length num filled with whitespace*)  
 let rec whitespace num = 
-  if num == 1 then " " 
+  if num = 1 then " " 
   else " " ^ whitespace (num-1)
 
-(*TODO: implement correctly*)
+(**[get_snake_seg snake i] gets the [i]th segment of [snake]. *)
+let get_snake_seg snake i=
+  List.nth snake i
+
+(**[get_seg_ycorr seg] gets the y corrdinate of the snake segment [seg]. *)
+let get_seg_ycorr seg =
+  List.nth seg 0
+
+(**[get_seg_xcorr seg] gets the leftmost x corrdinate of the snake segment [seg]. *)
+let get_seg_xcorr seg =
+  List.nth seg 1
+
+(** The format of each snake segment. *)
+let snake_seg =
+  "[]"
+
+let rec draw_snake_seg snake len i=
+  let seg = get_snake_seg snake i in
+  set_cursor (get_seg_xcorr seg) (get_seg_ycorr seg);
+  if i = len-1 then print_endline (snake_seg)
+  else (print_endline (snake_seg); draw_snake_seg snake len (i+1))
+
 (*draws snake segment on one row; len number of repetitions*)
-let rec draw_snake_seg len = 
-  if len == 1 then "o" 
-  else "o" ^ draw_snake_seg (len-1) (*for testing purposes*)
-(*failwith "unimplemented"*)
+let draw_snake snake = 
+  (* save_cursor(); *)
+  let len = List.length snake in
+  draw_snake_seg snake len 0
+(* restore_cursor() *)
 
 let draw_apple = 
   (*TODO: implement correctly*)
-  "u" (*for testing purposes*)
+  "o" (*for testing purposes*)
 (*failwith "unimplemented"*)
 
 (*checks if part of the snake is in the current row*)
 let check_snake row snake = 
-  if snake == [] then false
-  else row == List.nth (List.nth snake 0) 0
+  if snake = [] then false
+  else row = List.nth (List.nth snake 0) 0
 
 (*checks if the apple is in the current row*)
 let check_apple row apple = 
-  row == snd(apple)
+  row = snd(apple)
 
-(**snake is a list (described in make_snake), apple is a tuple (x, y) of apple location 
-   this may not be the best way to implement snake and apple so we can change it if
-   necessary. Maybe they can be included in the state variable st? I just added it 
-   now so that the function would return unit. 
+(**snake is a list (described in make_snake), apple is a tuple (x, y) of apple 
+   location this may not be the best way to implement snake and apple so we can 
+   change it if necessary. Maybe they can be included in the state variable st? 
+   I just added it now so that the function would return unit. 
 
    Problem: the function prints forever right now and i can't figure out why*)
 
 (*this function goes row by row and if the snake or the apple are in the current row, 
   it draws them and adds white space around them, and the board borders.*)
-let rec draw_internal st w l snake apple = 
-  let row = 
-    match (check_apple l apple , check_snake l snake) with
-    | (false, false) -> "|" ^ (whitespace (w)) ^ "|"
-    | (true, false) -> "|" ^ (whitespace (fst(apple)-1)) ^ (draw_apple) ^ (whitespace (w-fst(apple))) ^ "|"
-    | (x, true) -> 
-      let snake_x1 = List.nth (List.nth snake 0) 1 in 
-      let snake_x2 = List.nth (List.nth snake 0) 2 in
-      let len = snake_x2 - snake_x1 + 1 in
-      match x with 
-      | false -> "|" ^ (whitespace (snake_x1-1)) ^ (draw_snake_seg len) ^ 
-                 (whitespace (w-snake_x2)) ^ "|"
-      | true ->
-        if fst(apple) < snake_x1 then 
-          "|" ^ (whitespace (fst(apple)-1)) ^ (draw_apple) ^ (whitespace (snake_x1-fst(apple)-1)) ^ 
-          (draw_snake_seg len) ^ (whitespace (w-snake_x2)) ^ "|"
-        else 
-          "|" ^ (whitespace (snake_x1-1)) ^ (draw_snake_seg len) ^ (whitespace (fst(apple)-snake_x2-1)) 
-          ^ (draw_apple) ^ (whitespace (w-fst(apple))) ^ "|"
-    in 
+let rec draw_verti_edge w h = 
+  let row = "|" ^ (whitespace (w)) ^ "|"
+  in
+  if h = 1 then (print_endline row; print_endline) 
+  else (print_endline row; draw_verti_edge w (h-1))
 
-  let next_snake = 
-    match snake with
-    | [] -> []
-    | h :: t -> if check_snake l snake then t else snake in
-
-  if l == 1 then (print_endline row; st; print_endline) (*for some reason this never triggers*)
-  else (print_endline row; draw_internal st w (l-1) next_snake apple)
-
+(**[draw_horiz_edge w] draws the horizontal bondrary of the board. *)
 let rec draw_horiz_edge w  = 
-  if w == 1 then "-" 
+  if w = 1 then "-" 
   else "-" ^ draw_horiz_edge (w-1)
 
-(*w and l are the width and height of the playable area. so 0,0 is the first playable
-  pixel and w,l is the last*)
-let make_board st w l snake apple =
+(*w and h are the width and height of the playable area. so 0,0 is the first 
+  playable pixel and w,l is the last*)
+let make_board st w h snake apple =
   print_endline (" " ^ draw_horiz_edge (w));
-  draw_internal st w l snake apple;
-  print_endline (" " ^ draw_horiz_edge (w))
+  draw_verti_edge w h;
+  print_endline (" " ^ draw_horiz_edge (w));
+  save_cursor();
+  set_cursor (fst apple) (snd apple);
+  print_endline (draw_apple);
+  draw_snake snake;
+  restore_cursor()
+
+
 
 let play_game arg =
   (*testing make_board*)
-  make_board () 78 50 [[2;2;2]] (7,8)
+  make_board () 78 30 [[2;2];[2;4];[2;6]] (7,8)
 
 let main () = 
   ANSITerminal.(print_string[red] "\n\ Welcome to Snake! Press enter to start \n");
