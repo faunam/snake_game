fad8038a152e077896a6aef022507a8ea8c29952
Gemma Standley <gjs97@cornell.edu>
2019-04-17 12:23:17 -0400

    more tests

--- test.ml
+++ test.ml
@@ -67,6 +67,21 @@ let check_eat_tests = [
   make_check_eat_test "check_eat false" (3,3) [[1;1];[2;2]] false;
 ]
 
+let make_snake_add_head_test 
+  (name : string)
+  (dir : direction)
+  (snake : int list list)
+  (expected_output : int list list) : test =
+  name >:: (fun _ -> 
+    assert_equal expected_output (snake_add_head dir snake))
+
+let snake_add_head_tests = [
+  make_snake_add_head_test "snake_add_head up" Up [[2;2]] [[2;1];[2;2]];
+  make_snake_add_head_test "snake_add_head down" Down [[2;2]] [[2;3];[2;2]];
+  make_snake_add_head_test "snake_add_head left" Left [[3;3]] [[1;3];[3;3]];
+  make_snake_add_head_test "snake_add_head right" Right [[3;3]] [[5;3];[3;3]]
+]
+
 let tests = 
  "test suite  for A6" >::: List.flatten [
    whitespace_tests;
@@ -74,6 +89,7 @@ let tests =
    get_seg_ycorr_tests;
    get_seg_xcorr_tests;
    check_eat_tests;
+   snake_add_head_tests;
   
   ]
 
