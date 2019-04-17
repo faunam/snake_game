7e7da169792082ff4b5e1d15dd6b9014adcc6352
Gemma Standley <gjs97@cornell.edu>
2019-04-17 11:52:01 -0400

    more tests

--- test.ml
+++ test.ml
@@ -10,7 +10,10 @@ let make_whitespace_test
     assert_equal expected_output (whitespace num))
 
 let whitespace_tests = [
+  make_whitespace_test "whitespace 0" 0 "";
   make_whitespace_test "whitespace 1" 1 " ";
+  make_whitespace_test "whitespace 2" 2 "  ";
+  make_whitespace_test "whitespace 7" 7 "       ";
 ]
 
 let tests = 
