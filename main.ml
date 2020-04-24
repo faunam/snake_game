4d845079006c60ac1b472735fbf5967eb4c1e9c7
Fauna Mahootian <flm45@cornell.edu>
2020-04-23 18:45:56 -0700

    added more detailed instructions to intro screen

--- main.ml
+++ main.ml
@@ -16,8 +16,11 @@ let ter_wid = width + 2
 let main () = 
   Display.reset_terminal();
   resize ter_wid ter_hei;
-  ANSITerminal.(print_string[red] "\n\ Welcome to Snake! Use WASD to change 
-    direction. Press enter to start \n");
+  ANSITerminal.(print_string[red] "\n\ Welcome to Snake! 
+  Use WASD to change direction. 
+  Eat apples (red o's) to grow. 
+  Eat power apples (purples O's) before they shrink to grow faster. 
+  The longer the snake, the faster it moves. \n \n Press enter to start. \n");
   print_string[red] "> ";
   match read_line () with
   | exception _ -> ()
